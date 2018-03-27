90d9a17284bb3d4bdf05fd69c4e055a63f0c00b1
Jakub Lewandowski <kubik92@gmail.com>
2018-03-27 17:46:57 +0200

    szybko rozwiązana kata 27-03-2018

---
+++ Bit Counting/BitCounting/BitCounting/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = (int)new Decimal(1);
+            Console.WriteLine(CountBits( int.Parse(Console.ReadLine()) ) );
+            Console.ReadLine();
+        }
+
+        public static int CountBits(int n)
+        {
+            var i = 0;
+            var bitArr = new BitArray(new int[] { n });
+            foreach(bool bit in bitArr)
+            {
+                if (bit == true) i++;
+            }
+            return i;
+        }
+    }
+}
